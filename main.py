7b7c42ae301de463ed7921df082964136761d30a
parkwonhyoung <136056549+parkwonhyoung@users.noreply.github.com>
2023-06-10 17:45:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -9,31 +9,30 @@ import sympy
 # 전달함수 G(s)의 분자와 분모 계수
 s = sympy.symbols('s')
 
-Gnum= [100]
-Gden= [1,5,6]
+numG= [100]
+denG= [1,5,6]
 
-Hnum = [100]
-Hden = [1, 5, 106]
+numH = [100]
+denH= [1, 5, 106]
 
 # 폐루프 전달함수 H(s) 계산
 G = ( 100/( (s+2)*(s+3) ) )
 H = G / ( 1 + G )
 
-st.warning(f"폐 루프 전달함수 : ")
-st.write(H)
+st.write("폐 루프 전달함수 : ",H)
 
-st.error(f" 단위계단입력에 대한 응답곡선")
-G0 = signal.lti([100],[1]) # 비례요소
-G1 = signal.lti([1],[1,2])#적분요소
-G2 = signal.lti([1],[1,3])#1차지연요소
-G3 = signal.lti([100],[1,5,6])
+st.write(" \n 단위계단입력에 대한 응답곡선")
+G0 = signal.lti([100],[1])  # 비례요소
+G1 = signal.lti([1],[1,2] )  #적분요소
+G2 = signal.lti([1],[1,3])   #1차지연요소
+G3 = signal.lti([100],[1,5,6])  #Overall 
 
 
 t = np.linspace(0, 10, 1000)  # 시간 범위 설정
 u = np.ones_like(t)  # 단위계단입력 생성
 
 # 응답곡선 계산
-t, y = signal.step((Hnum, Hden), T=t)
+t, y = signal.step((numH, denH), T=t)
 
 # 응답곡선 그래프 그리기
 plt.plot(t, y)
@@ -45,14 +44,14 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 st.pyplot()
 
 
-st.success(f" 주파수 응답에 대한 보드선도")
+st.write(" \n 주파수 응답에 대한 보드선도")
 #주파수 범위 정의
 Frequency = np.logspace(-2,2,500)
 
 #주파수 응답 계산
 systems = [G0,G1,G2,G3]
 labels =['Proportional Element','Integral Element','First_Order Lag Element','Overall System']
-colors = ['r','g','m','b']
+colors = ['b','r','c','m']
 
 plt.figure(figsize=(12,8))
 
